14065fe8d945358f52eb499d204c28d67d796c7b
coolzoom <tommingc@Gmail.com>
2022-12-14 01:49:10 +0800

    fixes

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,5 +1,5 @@
 #ifndef __REVISION_DATA_H__
 #define __REVISION_DATA_H__
- #define REVISION_HASH                      "03dbca7a777e05655b43"
- #define REVISION_DATE                      "2022-12-14 01:21:20 +0800"
+ #define REVISION_HASH                      "07fdd25e9e12ad32dc51"
+ #define REVISION_DATE                      "2022-12-14 01:38:43 +0800"
 #endif // __REVISION_DATA_H__
